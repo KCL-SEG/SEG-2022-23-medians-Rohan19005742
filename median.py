e07c2e0a83b49264205c5431d36ecb147ec003a7
Rohan19005742 <60757115+Rohan19005742@users.noreply.github.com>
2022-10-13 15:32:57 +0100

    13/10/2022
    Completed Median

--- median.py
+++ median.py
@@ -9,4 +9,12 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
 print(numbers)
+numbers.sort()
+if (len(numbers)%2==0):
+    first_median = numbers[int(len(numbers)/2) - 1]
+    second_median = numbers[int(len(numbers)/2)]
+    print((first_median + second_median) / 2)
+else:
+    print(numbers[int(((len(numbers) - 1) / 2))])
